69324e4d197db295d313f0e287905c08458fd57f
patriceandrea <andrea_pavico98@yahoo.ca>
2022-05-05 17:57:23 +0000

    add signup form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   root to: 'products#index'
 
+
+
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -19,11 +22,21 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  
+  resource :users do
+    root to: 'users#new'
+  end
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, :categories, except: [:edit, :update, :show]
+    
   end
+
+
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
